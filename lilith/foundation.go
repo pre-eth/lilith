37bb634241f893d08c5ca0089cb989a9919d59c7
Preeth Vijay <preethvijay0@gmail.com>
2024-02-20 12:58:15 -0500

    update doc comments and implement some more internal functions for the cipher

--- lilith/foundation.go
+++ lilith/foundation.go
@@ -53,11 +53,57 @@ func swap(arr []uint, a uint, b uint) {
 	arr[a] ^= arr[b]
 }
 
-// The state and counter variables are initialized from the sub keys as follows:
-// For original piecewise function, see img/INIT_PIECEWISE.png
-func InitPiecewise(arr []uint16, h uint8, i uint8, j uint8) uint32 {
+/*
+	The state and counter variables are initialized from the sub keys as follows
+	For original piecewise function, see img/INIT_PIECEWISE.png
+
+	NOTE: param <arr> is guaranteed by caller to be at least of size 8
+*/
+func initPiecewise(arr []uint16, h uint8, i uint8, j uint8) uint32 {
 	num := uint32(arr[(i + j) & 7]) << 16
 	num |= uint32(arr[(h + j) & 7])
 	return num
 }
+
+// phi() determines the value of the counter carry bit used in ctr_system()
+// For original function that gives counter carry bit, see img/COUNTER_CARRY.png
+func phi(ctr uint32, j uint8, a uint32, cc *uint8) uint32 {
+	var sum uint64 = uint64(ctr + a + uint32(*cc))
+	if (sum >= U32_MAX) {
+		*cc = 1
+	} else {
+		*cc = 0
+	}
+	return uint32(*cc)
+}
+
+// The counter dynamics defined in Section 2.5, which you can also find in img/COUNTER_SYSTEM.png 
+func ctrSystem(ctr []uint32, _phi *uint8) {
+  ctr[0] = (ctr[0] + A0 + phi(ctr[0], 0, A0, _phi)) & (U32_MAX - 1)
+  ctr[1] = (ctr[1] + A1 + phi(ctr[1], 1, A1, _phi)) & (U32_MAX - 1)
+  ctr[2] = (ctr[2] + A2 + phi(ctr[2], 2, A2, _phi)) & (U32_MAX - 1)
+  ctr[3] = (ctr[3] + A3 + phi(ctr[3], 3, A3, _phi)) & (U32_MAX - 1)
+  ctr[4] = (ctr[4] + A4 + phi(ctr[4], 4, A4, _phi)) & (U32_MAX - 1)
+  ctr[5] = (ctr[5] + A5 + phi(ctr[5], 5, A5, _phi)) & (U32_MAX - 1)
+  ctr[6] = (ctr[6] + A6 + phi(ctr[6], 6, A6, _phi)) & (U32_MAX - 1)
+  ctr[7] = (ctr[7] + A7 + phi(ctr[7], 7, A7, _phi)) & (U32_MAX - 1)
+}
+
+// Represents ((x + j)^2 XOR (((x + j)^2) >> 32)) mod 2^32
+func gFunction(state []uint32, ctr []uint32, gfn []uint32) {
+	var i uint8 = 0
+	var tmp uint64
+	for i < 8 {
+		tmp = uint64(state[i]) + uint64(ctr[i])
+		tmp *= tmp
+		gfn[i] = uint32((tmp ^ (tmp >> 32)) & (U32_MAX - 1))
+	}
+}
+
+
+
+
+
+
+
 
